6a78c066e01aa4bdeba912dade871067ee893550
Ken Morishita <k_morishita@yumemi.co.jp>
2016-03-09 15:34:17 +0900

    add test for utf8 encoded message

--- test/validator.js
+++ test/validator.js
@@ -24,6 +24,20 @@ var chai = require('chai'),
         SubscribeURL: 'https://www.amazonaws.com',
         Type: 'SubscriptionConfirmation'
     }),
+    utf8Message = {
+        Type: 'Notification',
+        MessageId: '1',
+        TopicArn: 'arn',
+        Message: 'Ａ Ｍｅｓｓａｇｅ Ｆｏｒ ｙｏｕ！',
+        Timestamp: (new Date).toISOString(),
+        SignatureVersion: '1',
+        SigningCertURL: "https://localhost:56789/cert.pem"
+    },
+    utf8SubscriptionControlMessage = _.extend({}, utf8Message, {
+        Token: 'Nonce',
+        SubscribeURL: 'https://www.amazonaws.com',
+        Type: 'SubscriptionConfirmation'
+    }),
     validCertUrl = 'https://sns.us-east-1.amazonaws.com/cert.pem';
 
 describe('Message Validator', function () {
@@ -180,6 +194,42 @@ describe('Message Validator', function () {
         it('should accept a valid subscription control message', function (done) {
             (new MessageValidator(/^localhost:56789$/))
                 .validate(validSubscriptionControlMessage, done);
+        });
+    });
+
+    describe('UTF8 message validation', function () {
+        before(function (done) {
+            pem.createCertificate({}, function (err, certHash) {
+                if (err) throw err;
+
+                var crypto = require('crypto'),
+                    validMessages = [utf8Message, utf8SubscriptionControlMessage];
+
+                for (var i = 0; i < validMessages.length; i++) {
+                    var signer = crypto.createSign('RSA-SHA1');
+
+                    for (var j = 0; j < signableKeys.length; j++) {
+                        if (signableKeys[j] in validMessages[i]) {
+                            signer.update(signableKeys[j] + "\n"
+                                + validMessages[i][signableKeys[j]] + "\n", 'utf8');
+                        }
+                    }
+
+                    validMessages[i]['Signature']
+                        = signer.sign(certHash.serviceKey, 'base64');
+                }
+
+                MessageValidator.__set__('getCertificate', function (url, cb) {
+                    cb(null, certHash.certificate);
+                });
+
+                done();
+            });
+        });
+
+        it('should accept a valid UTF8 message', function (done) {
+            (new MessageValidator(/^localhost:56789$/, 'utf8'))
+                .validate(utf8Message, done);
         });
     });
 });
